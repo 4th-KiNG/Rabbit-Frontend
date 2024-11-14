ed3816dd5351b7714d72258465a55041d9f617d6
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-11-14 19:55:40 +0300

    review 2

--- src/share/components/Navbar/Navbar.tsx
+++ src/share/components/Navbar/Navbar.tsx
@@ -9,42 +9,49 @@ import {
 
 const Navbar = () => {
   return (
-    <div className="bg-[#2A2A2A] max-w-80 h-screen px-8 pb-8 pt-[116px] max-[1024px]:px-4 max-[1024px]:max-w-60">
-      {/* NavBar задается тегом nav, а в нем я обычно пишу span. в общем тег ul и li нужно поменять */}
-      <ul>
-        {/* чтобы не плодить кучу одинакового кода создаешь массив links, создаешь тип INavLink со свойствами title, url, startImg
+    <>
+      {/* чтобы не писать px-8 pb-8 pt-[116px] можешь написать p-8 pt-[116px] */}
+      <div className="bg-[#2A2A2A] max-w-80 h-screen px-8 pb-8 pt-[116px] max-[1024px]:px-4 max-[1024px]:max-w-60">
+        {/* NavBar задается тегом nav, а в нем я обычно пишу span. в общем тег ul и li нужно поменять */}
+        <ul>
+          {/* чтобы не плодить кучу одинакового кода создаешь массив links, создаешь тип INavLink со свойствами title, url, startImg
         и потом просто выводить все ссылки через links.map */}
-        {/* если закрывающийся тег не нужен, то можно просто писать <NavLink /> */}
-        {/* чтобы не писать mb-6 в каждом элементе, можешь просто у родительского элемента задать flex и сделать gap */}
-        <li className="mb-6">
-          <NavLink title={"Новости"} url={"./url"} startImg={news}></NavLink>
-        </li>
-        <li className="mb-6">
-          <NavLink title={"Профиль"} url={"./url"} startImg={profile}></NavLink>
-        </li>
-        <li className="mb-6">
-          <NavLink
-            title={"Подписчики"}
-            url={"./url"}
-            startImg={friends}
-          ></NavLink>
-        </li>
-        <li className="mb-6">
-          <NavLink
-            title={"Уведомления"}
-            url={"./url"}
-            startImg={notifications}
-          ></NavLink>
-        </li>
-        <li className="mb-6">
-          <NavLink
-            title={"Настройки"}
-            url={"./url"}
-            startImg={settings}
-          ></NavLink>
-        </li>
-      </ul>
-    </div>
+          {/* если закрывающийся тег не нужен, то можно просто писать <NavLink /> */}
+          {/* чтобы не писать mb-6 в каждом элементе, можешь просто у родительского элемента задать flex и сделать gap */}
+          <li className="mb-6">
+            <NavLink title={"Новости"} url={"./url"} startImg={news}></NavLink>
+          </li>
+          <li className="mb-6">
+            <NavLink
+              title={"Профиль"}
+              url={"./url"}
+              startImg={profile}
+            ></NavLink>
+          </li>
+          <li className="mb-6">
+            <NavLink
+              title={"Подписчики"}
+              url={"./url"}
+              startImg={friends}
+            ></NavLink>
+          </li>
+          <li className="mb-6">
+            <NavLink
+              title={"Уведомления"}
+              url={"./url"}
+              startImg={notifications}
+            ></NavLink>
+          </li>
+          <li className="mb-6">
+            <NavLink
+              title={"Настройки"}
+              url={"./url"}
+              startImg={settings}
+            ></NavLink>
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
