8da95c510e04e935f0c5023a75f978f9108eeea2
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2024-12-23 12:10:35 +0300

    fix conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-//import { useProfile } from "./lib/hooks/useProfile";
-import { PostsPage, ProfilePage } from "./pages";
+import { useProfile } from "./lib/hooks/useProfile";
+import { AuthPage, PostsPage, ProfilePage } from "./pages";
 import { Layout } from "./share";
 
 function App() {
-  //const { user } = useProfile();
-  // if (!user) {
-  //   return <AuthPage />;
-  // }
+  const { user } = useProfile();
+
+  if (!user) {
+    return <AuthPage />;
+  }
   return (
     <Layout>
       <Routes>
