654b35d126b1ffa35223fdd3ac141e7161428e85
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-01-26 19:16:14 +0300

    comment filters

--- src/pages/PostsPage/PostsPage.tsx
+++ src/pages/PostsPage/PostsPage.tsx
@@ -1,37 +1,6 @@
 import usePosts from "../../lib/hooks/usePosts";
-import { CreatePostArea, Post, Filters } from "../../share";
+import { CreatePostArea, Post } from "../../share";
 import { PostProps } from "../../types/post.types";
-
-// const posts: PostProps[] = [
-//   {
-//     id: "1",
-//     title: "Давайте обсудим",
-//     userId: "2",
-//     text: "Текст",
-//     createDate: new Date(),
-//   },
-//   {
-//     id: "1",
-//     title: "Давайте обсудим",
-//     userId: "2",
-//     text: "Очень много текста. Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.Очень много текста.",
-//     createDate: new Date(),
-//   },
-//   {
-//     id: "1",
-//     title: "Давайте обсудим",
-//     userId: "2",
-//     text: "Текст",
-//     createDate: new Date(),
-//   },
-//   {
-//     id: "1",
-//     title: "Давайте обсудим",
-//     userId: "2",
-//     text: "Текст",
-//     createDate: new Date(),
-//   },
-// ];
 
 const PostsPage = () => {
   const { posts } = usePosts();
@@ -39,7 +8,7 @@ const PostsPage = () => {
     <>
       <div className=" flex flex-col gap-8 w-full  max-[500px]:gap-3">
         <CreatePostArea />
-        <Filters />
+        {/* <Filters /> */}
         {posts &&
           posts.map((post: PostProps, index) => <Post {...post} key={index} />)}
       </div>
