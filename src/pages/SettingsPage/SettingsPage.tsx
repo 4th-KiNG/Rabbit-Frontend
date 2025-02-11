11813545d64159967f83802434e4c0d0c7d7d19f
Leshik <leshilmv@gmail.com>
2025-02-11 20:58:35 +0300

    corrected code

--- src/pages/SettingsPage/SettingsPage.tsx
+++ src/pages/SettingsPage/SettingsPage.tsx
@@ -3,7 +3,6 @@ import { SettingsInput, SettingsInputPasswword } from "./SettingsInput/SettingsI
 import { inputContent, passwordContent } from "./SettingsInput/SettingsInput.static"
 import { IInput } from "./SettingsInput/SettingsInput.types"
 import { Select, SelectItem } from "@nextui-org/react";
-import { isMobileSettings } from "../../utils/styles.utils"
 
 const genders = [
   {key: "female", label: "Женский"},
@@ -12,20 +11,17 @@ const genders = [
 const SettingsPage = () => {
   return (
     <>
-      <h1 className="text-[2.5rem] font-bold mb-8"
-      style={isMobileSettings() ? {fontSize: "1.5rem", marginBottom: "20px"} : {}}>Настройки</h1>
-      <form className="max-w-[664px] w-full gap-y-11 flex flex-col" action="#">
+      <h1 className="text-[2.5rem] font-bold mb-8 max-[475px]:text-[1.5rem] max-[475px]:mb-5">Настройки</h1>
+      <form className="max-w-[664px] w-full gap-y-11 flex flex-col max-[475px]:gap-y-8" action="#">
         <div>
-          <h2 className="text-[2.5rem] font-bold text-[#7F7F7F] mb-6"
-          style={isMobileSettings() ? {fontSize: "1.5rem"} : {}}>Личные данные</h2>
+          <h2 className="text-[2.5rem] font-bold text-[#7F7F7F] mb-6 max-[475px]:text-[1.5rem]">Личные данные</h2>
           <div className="flex flex-wrap gap-x-8 gap-y-4 max-[475px]:gap-x-4">
             {inputContent.map((input: IInput, index) => {
               return <SettingsInput {...input} key={index} />
             })}
             <Select labelPlacement="outside" label="Пол" placeholder="Пол" 
-            className="max-w-[200px] [&_svg]:scale-150 [&_label]:translate-x-4 
-            [&_button[data-slot='trigger']]:rounded-[62px] [&_button[data-slot='trigger']]:px-4" 
-            style={isMobileSettings() ? {maxWidth: "156px"} : {}}>
+            className="max-w-[200px] max-[475px]:max-w-[156px] [&_svg]:scale-150 [&_label]:translate-x-4 
+            [&_button[data-slot='trigger']]:rounded-[62px] [&_button[data-slot='trigger']]:px-4">
               {genders.map((gender) => {
                 return <SelectItem key={gender.key}>{gender.label}</SelectItem>
               })}
@@ -37,7 +33,7 @@ const SettingsPage = () => {
           </Button>
         </div>
         <div>
-          <h2 className="text-[2.5rem] font-bold text-[#7F7F7F] mb-6">Пароль</h2>
+          <h2 className="text-[2.5rem] font-bold text-[#7F7F7F] mb-6 max-[475px]:text-[1.5rem]">Пароль</h2>
           <div className="flex flex-wrap gap-x-8 gap-y-4 max-[475px]:gap-x-4">
             {passwordContent.map((item: string, index) => {
               return <SettingsInputPasswword label={item} key={index}/>
