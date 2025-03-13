e02ebb0da2a73acce89af49b6eb11485d2241a82
4th-KiNG <95131486+4th-KiNG@users.noreply.github.com>
2025-03-13 17:36:51 +0300

    add user auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
-//import { useProfile } from "./lib/hooks/useProfile";
+import { useProfile } from "./lib/hooks/useProfile";
 import {
-  //AuthPage,
+  AuthPage,
   NotificationPage,
   PostsPage,
   ProfilePage,
@@ -12,10 +12,10 @@ import {
 import { Layout } from "./share";
 
 function App() {
-  // const { user } = useProfile();
-  // if (!user) {
-  //   return <AuthPage />;
-  // }
+  const { user } = useProfile();
+  if (!user) {
+    return <AuthPage />;
+  }
   return (
     <Layout>
       <Routes>
